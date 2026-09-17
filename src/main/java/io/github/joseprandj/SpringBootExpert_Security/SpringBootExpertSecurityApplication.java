c817e3b4b36061882d227d0d6a837ff8fa8fb294
JJ <juniorj53@gmail.com>
2026-09-17 15:55:34 -0300

    Init

---
+++ src/main/java/io/github/joseprandj/SpringBootExpert_Security/SpringBootExpertSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.github.joseprandj.SpringBootExpert_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpertSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpertSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/joseprandj/SpringBootExpert_Security/SpringBootExpertSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joseprandj.SpringBootExpert_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpertSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
